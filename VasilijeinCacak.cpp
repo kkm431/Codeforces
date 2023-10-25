ed0b292cb588059e3ebbcfa5b04a2bea6f08db16
Ketan <ketanmishra1998@gmail.com>
2023-10-25 11:50:39 -0700

    first Push

---
+++ VasilijeinCacak.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include <vector>
+#include<deque>
+
+using namespace std;
+
+int main(){
+    
+    vector<int> out;
+    int n,k,x;
+    int t;
+    cint>>t;
+    while(t--){
+        cin>>n>>k>>x;
+
+
+        for(int i=1; i<=n; i++){
+            out.push_back(i);
+        
+        int sum=0;
+
+        for(int i=0;)
+
+    }
+
+    }
+    
+    
+
+}
